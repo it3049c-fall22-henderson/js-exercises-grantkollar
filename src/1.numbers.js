52cd09699c47c82a732a137302e31b0e4d57c6dd
grantkollar <89742140+grantkollar@users.noreply.github.com>
2022-09-04 19:07:33 -0400

    finally fixed the parseInt function and all tests

--- src/1.numbers.js
+++ src/1.numbers.js
@@ -15,7 +15,7 @@ function multiply(num1, num2) {
 
 //parseInt: should use parseInt correctly to convert strings to integer numbers
 function parseInt (str) {
-  return Number(str);
+  return Number(parseFloat(str).toFixed(0));
 }
 
 //addAndReturn2DecimalPlaces: should return a number value, rounded to 2 decimal places
