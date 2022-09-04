5f9f0fa59810104c5b2fa596adcfa38841ad1255
grantkollar <89742140+grantkollar@users.noreply.github.com>
2022-09-04 18:01:24 -0400

    arrays sum created and working

--- src/3.arrays.js
+++ src/3.arrays.js
@@ -22,7 +22,11 @@ function indexOf(arr, item) {
 // Sum: you should be able to sum the items of an array
 // Example: arr = [1,2,3]   =>  6
 function sum(arr) {
-  return ;
+  let sum = 0;
+  arr.forEach(element => {
+    sum += element;
+  });
+  return sum;
 }
 
 // Filter: Should filter out all instances of a value from an array
